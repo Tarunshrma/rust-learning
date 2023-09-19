2d907994c81e139f0df1fa3828a3401429080e0b
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2023-09-19 12:42:05 +0400

    first rust program

---
+++ chapter-1/hello-world.rs
@@ -0,0 +1,7 @@
+fn main() {
+    //note1:  ! mark here for println because it is a macro not a function.
+    //if it would have been function we should have called like println("")
+
+    //note2: no semi coloumn at end.
+    println!("Hello Rust World..");
+}
