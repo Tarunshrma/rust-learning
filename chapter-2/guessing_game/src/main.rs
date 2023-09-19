1481829adb15d6885cdcb97498b9aee77b77b4a8
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2023-09-19 13:48:00 +0400

    Added comments

--- chapter-2/guessing_game/src/main.rs
+++ chapter-2/guessing_game/src/main.rs
@@ -5,8 +5,11 @@ fn main() {
 
     println!("Guess a Number....");
 
+    //new is like static function on type string
+    //mut means this variable can be mutated...
     let mut str = String::new();
 
+    //& is refernce of variable str
     io::stdin().read_line(&mut str).expect("Expected a number!");
 
     println!("Your guess is {str}");
