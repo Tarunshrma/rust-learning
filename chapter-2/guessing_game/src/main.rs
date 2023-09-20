1e862df99f745fa895fbe77d9dd0a4f569400a40
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2023-09-20 12:45:11 +0400

    completed guess number game to learn rust

--- chapter-2/guessing_game/src/main.rs
+++ chapter-2/guessing_game/src/main.rs
@@ -1,16 +1,42 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Gauessing Game !!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    //    println!("Secret number is {secret_number}");
     println!("Guess a Number....");
 
-    //new is like static function on type string
-    //mut means this variable can be mutated...
-    let mut str = String::new();
+    loop {
+        //Note: new is like static function on type string
+        //Note: mut means this variable can be mutated...
+        let mut guess = String::new();
 
-    //& is refernce of variable str
-    io::stdin().read_line(&mut str).expect("Expected a number!");
+        //Note: & is refernce of variable str
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Expected a number!");
 
-    println!("Your guess is {str}");
+        //NOTE: guess variable can be redeclaredwith the different type this feature is called "Shadowing"
+        //NOTE: parse will convert the string to expected type.. e.g. u32 in this case
+        //NOTE: is string can not be converted to number, error can be handled
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        //NOTE: Compare & Match
+        //NOTE: Looks like switch case
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
